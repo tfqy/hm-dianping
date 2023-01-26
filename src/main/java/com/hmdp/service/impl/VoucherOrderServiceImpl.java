8f1911fc29edf3f38b4e2140597bd95beadeb990
tfqy <1115142237@qq.com>
2023-01-26 13:50:38 +0800

    单个JVM一人一单

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -9,6 +9,7 @@ import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,7 +36,6 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     }
 
     @Override
-    @Transactional
     public Result seckillVoucher(Long voucherId) {
         // 1.查询优惠券
         SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
@@ -54,9 +54,30 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             // 库存不足
             return Result.fail("库存不足！");
         }
-        //5，扣减库存
+        Long userId = UserHolder.getUser().getId();
+        //对用户id加锁，保证一人一单
+        synchronized (userId.toString().intern()) {
+            // 获取代理对象（因为在代理对象中开启了事务，所以要获取代理对象）
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+
+    }
+
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        // 5.用户id
+        Long userId = UserHolder.getUser().getId();
+        // 5.1一人一单
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        //5.2判断是否已经购买过
+        if (count > 0) {
+            //已经购买过
+            return Result.fail("您已经购买过一次！");
+        }
+        //6.扣减库存
         boolean success = seckillVoucherService.update()
-//                .set(true, "stock", voucher.getStock() - 1)   //如果使用这个方法，会出现并发问题，因为这个方法是先查询再更新，会导致更新的时候库存已经被别人更新了
+                //                .set(true, "stock", voucher.getStock() - 1)   //如果使用这个方法，会出现并发问题，因为这个方法是先查询再更新，会导致更新的时候库存已经被别人更新了
                 .setSql("stock = stock -1") //这个方法是直接执行sql语句，不会出现并发问题，但是这个方法不会判断库存是否为0，所以需要在下面判断库存是否为0
                 .eq("voucher_id", voucherId)
                 .gt("stock", 0)
@@ -65,19 +86,15 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             //扣减库存
             return Result.fail("库存不足！");
         }
-        //6.创建订单
+        //7.创建订单
         VoucherOrder voucherOrder = new VoucherOrder();
-        // 6.1.订单id
+        // 7.1.订单id
         long orderId = redisIdWorker.nextId("order");
         voucherOrder.setId(orderId);
-        // 6.2.用户id
-        Long userId = UserHolder.getUser().getId();
         voucherOrder.setUserId(userId);
-        // 6.3.代金券id
+        // 7.3.代金券id
         voucherOrder.setVoucherId(voucherId);
         save(voucherOrder);
-
         return Result.ok(orderId);
-
     }
 }
